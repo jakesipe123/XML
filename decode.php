b6d96d8a0d7c3687d29021317dcc44c0c3091954
jakesipe123 <60919213+jakesipe123@users.noreply.github.com>
2020-02-11 20:03:20 +0800

    Update decode.php

--- decode.php
+++ decode.php
@@ -5,10 +5,10 @@ $db_username = 'admin';
 $db_password = 'root1234';
 $db_hostname = 'dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com';
 $db_port = '3306';
-$db_name = 'db_1822038';
+$db_name = 'db_1822002';
 
 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
-$q = "SELECT * FROM NBA";
+$q = "SELECT * FROM Erp";
 $r = mysqli_query($conn, $q);
 
 while ($item = mysqli_fetch_object($r)) {
@@ -16,10 +16,9 @@ while ($item = mysqli_fetch_object($r)) {
 	$dec = json_decode($json, true);
 	
 	echo "<ul>";
-	echo "<li>". $dec["Top"] ."</li>";
-	echo "<li>". $dec["Name"]." </li>";
-	echo "<li>". $dec["Score"] ."</li>";
-	echo "<li>". $dec["Team"] ."</li>";
+	echo "<li>". $dec["Name"] ."</li>";
+	echo "<li>". $dec["Age"]." </li>";
+	echo "<li>". $dec["Gender"] ."</li>";
 	echo "</ul>";
 	echo "</ul>";
 }
